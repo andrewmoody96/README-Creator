9a9807d2f13ca50dcc45b6bb87e3e62e6963773f
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-03-31 02:16:16 -0500

    Inquirer takes and console logs user input.

--- Develop/index.js
+++ Develop/index.js
@@ -1,25 +1,116 @@
 // TODO: Include packages needed for this application
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const inquirer = require("inquirer");
+
 
 // TODO: Create an array of questions for user input
-const questions = ['Title: What is the title of your application?', 'Description: Please describe your application.', 'Installation: How do you install the application', 'Usage: How do you use it?', 'Contributing: How can other users contribute to this project?', 'Tests: How can users test your application?'];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your application?",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: "Please describe your application.",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "Installation: How do you install the application?",
+    name: "Installation",
+  },
+  { type: "input", message: "Usage: How do you use it?", name: "Usage" },
+  {
+    type: "input",
+    message: "Contributing: How can other users contribute to this project?",
+    name: "Contributing",
+  },
+  {
+    type: "input",
+    message: "Tests: How can users test your application?",
+    name: "Questions",
+  },
+];
+
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    console.log(answers);
+  });
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+// Currently generates a README with a string literal. This string literal is mostly markdown with placeholder input variable names.
+function writeToFile(fileName, data) {
+  const fs = require("fs");
+
+  const content = `# USER TITLE
+---
+
+## ${userDescription}
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi vitae suscipit tellus mauris.
+
+Arcu dui vivamus arcu felis bibendum ut tristique et. Laoreet suspendisse interdum consectetur libero id faucibus nisl.
+
+---
+## ${userTOC}
+
+${userTOC}
+
+---
+## Installation
+
+${userInstallation}
+
+---
+## Usage
+
+${userUsage}
+
+---
+## License
+
+${userLicense}
+
+---
+## Contribution
+
+${userContribution}
+
+---
+## TESTS
+
+${userTests}
+
+---
+## Questions
+
+${userQuestions}
+
+<br>
+<br>
+
+---`;
+
+  fs.writeFile(
+    "/Users/andrewmoody/Downloads/README-Output/testREADME.md",
+    content,
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Saved");
+
+      //file written successfully
+    }
+  );
+}
+
+// writeToFile();
 
 // TODO: Create a function to initialize app
 function init() {}
